28b063c4d213e6d83e56d28b0a4674cce0b26a7b
devroa <roa.alejandra02@gmail.com>
2026-06-14 20:09:36 -0400

    se agrego dependencia OpenFeign para comunicacion entre microservicios

--- ms-pedido/src/main/java/cl/dsy1103/ms_pedido/MsPedidoApplication.java
+++ ms-pedido/src/main/java/cl/dsy1103/ms_pedido/MsPedidoApplication.java
@@ -3,9 +3,11 @@ package cl.dsy1103.ms_pedido;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class MsPedidoApplication {
 
 	public static void main(String[] args) {
